db7fe82a71200c6e8d8883bdc8c8955a3f2ac237
Victor Senna <vhsenna@gmail.com>
2022-03-29 20:20:06 -0300

    Add url patterns for views

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+]
