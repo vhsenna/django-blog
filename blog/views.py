f235c28b0013cff4d8975d91ec1b1fec69ddb3bf
Victor Senna <vhsenna@gmail.com>
2022-03-31 16:08:13 -0300

    Handle ModelForms in views

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from blog.models import Post
-from blog.forms import EmailPostForm
+from blog.models import *
+from blog.forms import EmailPostForm, CommentForm
 
 def post_list(request):
     object_list = Post.published.all()
@@ -19,11 +19,36 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/list.html', {
         'page': page,
-        'posts': posts})
+        'posts': posts
+    })
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/detail.html', {'post': post})
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+        else:
+            comment_form = CommentForm()
+
+    return render(request, 'blog/detail.html', {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form
+    })
 
 class PostListView(ListView):
     queryset = Post.published.all()
