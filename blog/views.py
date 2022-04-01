c54082578fdbb6bea861b2305ee43b795ab1ff48
Victor Senna <vhsenna@gmail.com>
2022-04-01 00:05:28 -0300

    Retrieve posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
 from django.core.mail import send_mail
-from blog.models import *
-from blog.forms import EmailPostForm, CommentForm
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 from taggit.models import Tag
+from blog.forms import CommentForm, EmailPostForm
+from blog.models import *
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -51,11 +52,17 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', 'publish')[:4]
+
     return render(request, 'blog/detail.html', {
         'post': post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
     })
 
 class PostListView(ListView):
